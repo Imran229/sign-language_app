6de29a4b2135d78be3f9cb2ee250f9b63e94ba1d
MD IMRAN <145384054+Imran229@users.noreply.github.com>
2025-10-22 18:46:27 +0530

    Change default port from 5000 to 10000

--- app.py
+++ app.py
@@ -124,6 +124,6 @@ def video_feed():
 # -----------------------------
 if __name__ == '__main__':
     # Get the port from the environment variable Render sets
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     # Run the app, listening on all available network interfaces
     app.run(host='0.0.0.0', port=port)
